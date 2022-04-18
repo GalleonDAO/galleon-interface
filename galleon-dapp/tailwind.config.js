10ecd246cb388c7f4d9e8c44f766b5b31cd874eb
Andrew <andywilkinson1993@gmail.com>
2022-04-18 09:09:49 +0100

    starting theme

--- galleon-dapp/tailwind.config.js
+++ galleon-dapp/tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,,ts,tsx}"],
+  content: ['./src/**/*.{html,js,jsx,,ts,tsx}'],
   theme: {
+    colors: {
+      theme: '#1fb6ff',
+      purple: '#7e5bef',
+      pink: '#ff49db',
+      orange: '#ff7849',
+      green: '#13ce66',
+      yellow: '#ffc82c',
+      'gray-dark': '#273444',
+      gray: '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      display: ['Morion'],
+      body: ['Wigrum'],
+    },
     extend: {},
   },
-  plugins: [require("@tailwindcss/aspect-ratio")],
-};
+  plugins: [require('@tailwindcss/aspect-ratio')],
+}
