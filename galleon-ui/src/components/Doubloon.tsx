f59aa4db6df3c1000b5afdad95fecbadde71aa35
Davy jones <hello@galleon.community>
2022-05-20 18:24:03 -0400

    removed DBL button

--- galleon-ui/src/components/Doubloon.tsx
+++ galleon-ui/src/components/Doubloon.tsx
@@ -1,6 +1,6 @@
-import Navigation from "components/Navigation";
-import { Fragment } from "react";
-import { Popover, Transition, Disclosure } from "@headlessui/react";
+import Navigation from 'components/Navigation'
+import { Fragment } from 'react'
+import { Popover, Transition, Disclosure } from '@headlessui/react'
 import {
   CloudUploadIcon,
   CogIcon,
@@ -10,27 +10,27 @@ import {
   ServerIcon,
   ShieldCheckIcon,
   XIcon,
-} from "@heroicons/react/outline";
-import { ChevronRightIcon, ExternalLinkIcon } from "@heroicons/react/solid";
-import crewBackground from "assets/brand/crew-bg.png";
-import { ChevronDownIcon } from "@heroicons/react/outline";
-import ResourcesIcon from "assets/brand/Resources-Icon.png";
-import CommunityIcon from "assets/brand/Community-Icon.png";
-import AboutIcon from "assets/brand/About-Icon.png";
-import ApplicationIcon from "assets/brand/Application-Icon.png";
-import GovernanceIcon from "assets/brand/Governance-Icon.png";
-import ProductsIcon from "assets/brand/Products-Icon.png";
-import TreasuryIcon from "assets/brand/Treasury-Icon.png";
-import GalleonLogo from "assets/brand/Union-Logo-Light.png";
+} from '@heroicons/react/outline'
+import { ChevronRightIcon, ExternalLinkIcon } from '@heroicons/react/solid'
+import crewBackground from 'assets/brand/crew-bg.png'
+import { ChevronDownIcon } from '@heroicons/react/outline'
+import ResourcesIcon from 'assets/brand/Resources-Icon.png'
+import CommunityIcon from 'assets/brand/Community-Icon.png'
+import AboutIcon from 'assets/brand/About-Icon.png'
+import ApplicationIcon from 'assets/brand/Application-Icon.png'
+import GovernanceIcon from 'assets/brand/Governance-Icon.png'
+import ProductsIcon from 'assets/brand/Products-Icon.png'
+import TreasuryIcon from 'assets/brand/Treasury-Icon.png'
+import GalleonLogo from 'assets/brand/Union-Logo-Light.png'
 
-import ResourcesIconDark from "assets/brand/Resources-Icon-Dark.png";
-import CommunityIconDark from "assets/brand/Community-Icon-Dark.png";
-import AboutIconDark from "assets/brand/About-Icon-Dark.png";
-import ApplicationIconDark from "assets/brand/Application-Icon-Dark.png";
-import GovernanceIconDark from "assets/brand/Governance-Icon-Dark.png";
-import ProductsIconDark from "assets/brand/Products-Icon-Dark.png";
-import TreasuryIconDark from "assets/brand/Treasury-Icon-Dark.png";
-import { classNames } from "utils";
+import ResourcesIconDark from 'assets/brand/Resources-Icon-Dark.png'
+import CommunityIconDark from 'assets/brand/Community-Icon-Dark.png'
+import AboutIconDark from 'assets/brand/About-Icon-Dark.png'
+import ApplicationIconDark from 'assets/brand/Application-Icon-Dark.png'
+import GovernanceIconDark from 'assets/brand/Governance-Icon-Dark.png'
+import ProductsIconDark from 'assets/brand/Products-Icon-Dark.png'
+import TreasuryIconDark from 'assets/brand/Treasury-Icon-Dark.png'
+import { classNames } from 'utils'
 
 const Doubloon = () => {
   return (
@@ -57,20 +57,20 @@ const Doubloon = () => {
                 Uniswap on the Arbitrum network.
               </p>
 
-              <a
+              {/* <a
                 href="https://app.galleon.community/dbl"
-                target={"_blank"}
+                target={'_blank'}
                 className="block text-center m-auto mt-10 py-4 md:w-1/2 w-full text-xl px-4 mb-10 rounded-2xl border-2 shadow border-theme-navy hover:border-theme-champagne hover:text-theme-champagne  bg-theme-champagne hover:bg-theme-navy text-theme-navy focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-theme-sky focus:ring-offset-theme-black"
                 rel="noreferrer"
               >
-                Buy Doubloon ($DBL)
-              </a>
+                Doubloon ($DBL)
+              </a> */}
             </div>
           </div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Doubloon;
+export default Doubloon
