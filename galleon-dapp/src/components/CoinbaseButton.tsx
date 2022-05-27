c1037e635ae330350b79e1d1962737751386255c
Andrew Wilkinson <andywilkinson1993@gmail.com>
2022-05-27 12:06:32 +0100

    Update CoinbaseButton.tsx

--- galleon-dapp/src/components/CoinbaseButton.tsx
+++ galleon-dapp/src/components/CoinbaseButton.tsx
@@ -3,6 +3,7 @@ import { initOnRamp } from '@coinbase/cbpay-js'
 import { useEthers } from '@usedapp/core'
 import coinbase from 'assets/coinbase-logo.png'
 const CoinbaseButton = () => {
+  const [isReady, setIsReady] = useState(false)
   const [cb, setCb] = useState(null)
   const { account } = useEthers()
   useEffect(() => {
@@ -21,18 +22,20 @@ const CoinbaseButton = () => {
         appId: process.env.REACT_APP_COINBASE_APP_ID,
         onReady: () => {
           console.log('ready')
+          setIsReady(true)
         },
         onSuccess: () => {
           console.log('success')
         },
         onExit: () => {
+          window.location.reload()
           console.log('exit')
         },
         onEvent: (event) => {
           console.log('ON RAMP EVENT: ', event)
         },
         experienceLoggedIn: 'embedded',
-        experienceLoggedOut: 'embedded',
+        experienceLoggedOut: 'popup',
         closeOnExit: true,
         closeOnSuccess: true,
       }),
@@ -46,13 +49,15 @@ const CoinbaseButton = () => {
   }
 
   return (
-    <button className="px-4 -ml-4 py-1.5" onClick={handleClick}>
-      <img
-        src={coinbase}
-        className=" inline-flex -translate-y-0.5 mr-1.5 h-6 w-6 text-theme-white"
-      ></img>
-      Buy ETH
-    </button>
+    isReady && (
+      <button className="px-4 -ml-4 py-1.5" onClick={handleClick}>
+        <img
+          src={coinbase}
+          className=" inline-flex -translate-y-0.5 mr-1.5 h-6 w-6 text-theme-white"
+        ></img>
+        Buy ETH
+      </button>
+    )
   )
 }
 
