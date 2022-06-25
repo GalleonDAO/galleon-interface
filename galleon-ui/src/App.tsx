96e2f89ae16e33995e60a3ff183893d2316e57a1
crown_dev <crown@galleon.community>
2022-06-25 19:54:54 +0100

    visit logging

--- galleon-ui/src/App.tsx
+++ galleon-ui/src/App.tsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
-
-import Navigation from "components/Navigation";
+import { useLogging } from "hooks/useLogging";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
+  const { logCounter, KNOWN_LABELS } = useLogging();
+  const location = useLocation();
+
+  logCounter(KNOWN_LABELS.VISIT, { path: location.pathname });
   return (
     <>
       <Outlet />
