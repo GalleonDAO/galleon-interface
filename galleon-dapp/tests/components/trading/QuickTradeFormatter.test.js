05b06c8b0d1e15a669ac4a036c6dfa47bdb66efd
crown_dev <crown@galleon.community>
2022-06-29 15:49:25 +0100

    first pass unit tests

--- galleon-dapp/tests/components/trading/QuickTradeFormatter.test.js
+++ galleon-dapp/tests/components/trading/QuickTradeFormatter.test.js
@@ -1,10 +1,21 @@
 const { colors } = require("../../../src/styles/colors");
-const { DoubloonToken } = require("../../../src/constants/tokens");
+const { DoubloonToken, ETH } = require("../../../src/constants/tokens");
+
 const {
   getPriceImpactColorCoding,
   getPriceImpact,
   formattedBalance,
-} = require("../../../src/components/trading/modules/QuickTradeFormatter", () => {});
+  getFormattedOuputTokenAmount,
+  formattedFiat,
+  getFormattedPriceImpact,
+  getHasInsufficientFunds,
+  getTransactionAmount,
+  getReceivedAmount,
+} = require("../../../src/components/trading/modules/QuickTradeFormatter");
+
+const { displayFromWei } = require("../../../src/utils");
+jest.mock("../../../src/utils");
+jest.mock("../../../src/components/trading/modules/QuickTradeFormatter");
 
 describe("getPriceImpactColorCoding", () => {
   test("Price Impact below lower limit -- returns copper", () => {
@@ -55,54 +66,119 @@ describe("formattedBalance", () => {
 
 describe("getFormattedOuputTokenAmount", () => {
   describe("IS Exchange Issuance", () => {
-    test("EI output amount IS undefined -- returns 0.0", () => {});
+    const isEI = true;
+    test("EI output amount IS undefined -- returns 0.0", () => {
+      const result = getFormattedOuputTokenAmount(isEI, 2);
+      expect(result).toBe("0.0");
+    });
     describe("EI output amount IS NOT undefined", () => {
-      test("displayFromWei returns null -- returns 0.0", () => {});
-      test("displayFromWei returns value -- returns value", () => {});
+      test("displayFromWei returns null -- returns 0.0", () => {
+        displayFromWei.mockImplementation(() => null);
+        const result = getFormattedOuputTokenAmount(isEI, 2, undefined, 10);
+        expect(result).toBe("0.0");
+      });
+      test("displayFromWei returns value -- returns value", () => {
+        displayFromWei.mockImplementation(() => "null");
+        const result = getFormattedOuputTokenAmount(isEI, 2, undefined, 10);
+        expect(result).toBe("10.0");
+      });
     });
   });
   describe("IS NOT Exchange Issuance", () => {
-    test("trade output amount IS undefined -- returns 0.0", () => {});
+    const isEI = false;
+    test("trade output amount IS undefined -- returns 0.0", () => {
+      const result = getFormattedOuputTokenAmount(isEI, 2);
+      expect(result).toBe("0.0");
+    });
     describe("trade output amount IS NOT undefined", () => {
-      test("displayFromWei returns null -- returns 0.0", () => {});
-      test("displayFromWei returns value -- returns value", () => {});
+      test("displayFromWei returns null -- returns 0.0", () => {
+        displayFromWei.mockImplementation(() => null);
+        const result = getFormattedOuputTokenAmount(isEI, 2, 10, undefined);
+        expect(result).toBe("0.0");
+      });
+      test("displayFromWei returns value -- returns value", () => {
+        displayFromWei.mockImplementation(() => "null");
+        const result = getFormattedOuputTokenAmount(isEI, 2, 10, undefined);
+        expect(result).toBe("10.0");
+      });
     });
   });
 });
 
 describe("formattedFiat", () => {
-  test("token balance IS undefined -- returns 0.00", () => {}); //TODO: Not implemented, confirm with Davy
-  test("token price IS undefined -- returns 0.00", () => {}); //TODO: Not implemented, confirm with Davy
-  test("Happy Path -- returns (tokenAmount * tokenPrice) AS (US) string", () => {});
+  test("token amount IS undefined -- returns 0.00", () => {
+    const result = formattedFiat(undefined, 10);
+    expect(result).toBe("0.0");
+  }); //TODO: Not implemented, confirm with Davy
+  test("token price IS undefined -- returns 0.00", () => {
+    const result = formattedFiat(1, undefined);
+    expect(result).toBe("0.0");
+  }); //TODO: Not implemented, confirm with Davy
+  test("Happy Path -- returns (tokenAmount * tokenPrice) AS (US) string", () => {
+    const expectedResult = "$10";
+    const result = formattedFiat(1, 10);
+    expect(result).toBe(expectedResult);
+  });
 });
 
 //TODO: this method lacks a lot of null handling
 describe("getFormattedPriceImpact", () => {
-  test("priceImpact IS undefined -- returns null", () => {});
-  test(
-    "priceImpact IS NOT undefined -- returns {priceImpact: (priceImpact TO 2DP), colorCoding: (colorCoding)}"
-  );
+  test("priceImpact IS undefined -- returns null", () => {
+    getPriceImpact.mockImplementation(() => undefined);
+    const result = getFormattedPriceImpact(1, 10, 1, 10);
+    expect(result).toBe(null);
+  });
+  test("priceImpact IS NOT undefined -- returns {priceImpact: (priceImpact TO 2DP), colorCoding: (colorCoding)}", () => {
+    getPriceImpact.mockImplementation(() => 1.01234);
+    getPriceImpactColorCoding.mockImplementation(() => colors.themeNavy);
+
+    const expectedResult = {
+      priceImpact: "(1.01$)",
+      colorCoding: colors.themeNavy,
+    };
+    const result = getFormattedPriceImpact(1, 10, 1, 10);
+
+    expect(result).toBe(expectedResult);
+  });
 });
 
 //TODO: this name breaks boolean convention, should assume the positive and confirm the negative
 describe("getHasInsufficientFunds", () => {
   describe("Input checks", () => {
-    test("bestOptionUnavailable IS FALSE -- returns false", () => {});
-    test("sellAmount IS (Zero) -- returns false", () => {});
-    test("sellAmount IS (Negative) -- returns false", () => {});
-    test("sellTokenBalance IS undefined -- returns false", () => {});
+    test("bestOptionUnavailable IS TRUE -- returns false", () => {
+      const result = getHasInsufficientFunds(true, 1, 10);
+      expect(result).toBe(false);
+    });
+    test("sellAmount IS (Zero) -- returns false", () => {
+      const result = getHasInsufficientFunds(false, 0, 10);
+      expect(result).toBe(false);
+    });
+    test("sellAmount IS (Negative) -- returns false", () => {
+      const result = getHasInsufficientFunds(false, -1, 10);
+      expect(result).toBe(false);
+    });
+    test("sellTokenBalance IS undefined -- returns false", () => {
+      const result = getHasInsufficientFunds(false, 1, undefined);
+      expect(result).toBe(false);
+    });
   });
   describe("Happy Path", () => {
-    test("sellTokenAmount > sellTokenBalance -- returns true", () => {});
-    test("sellTokenAmount <= sellTokenBalance -- returns false", () => {});
+    test("sellTokenAmount > sellTokenBalance -- returns true", () => {
+      const result = getHasInsufficientFunds(false, 10, 1);
+      expect(result).toBe(true);
+    });
+    test("sellTokenAmount <= sellTokenBalance -- returns false", () => {
+      const result = getHasInsufficientFunds(false, 1, 10);
+      expect(result).toBe(false);
+    });
   });
 });
 
 //TODO: SUGGESTED REFACTOR
-describe("HasSufficientFunds", () => {
+describe("hasSufficientFunds", () => {
   describe("Input checks", () => {
     //TODO: should invalid inputs yield sufficient funds?
-    test("bestOptionUnavailable IS FALSE -- returns true", () => {});
+    test("bestOptionUnavailable IS TRUE -- returns true", () => {});
     test("sellAmount IS (Zero) -- returns true", () => {});
     test("sellAmount IS (Negative) -- returns true", () => {});
     test("sellTokenBalance IS undefined -- returns true", () => {});
@@ -115,14 +191,30 @@ describe("HasSufficientFunds", () => {
 
 //TODO: Missing null check on symbol
 describe("getTransactionAmount", () => {
-  test("IS buying -- returns maximum payment", () => {});
-  test("IS NOT buying -- returns minimum received payment", () => {});
+  test("IS buying -- returns maximum payment", () => {
+    const expectedResult = "Maximum ETH Payment";
+    const result = getTransactionAmount(true, DoubloonToken, ETH);
+    expect(result).toBe(expectedResult);
+  });
+  test("IS NOT buying -- returns minimum received payment", () => {
+    const expectedResult = "Minimum DBL Received";
+    const result = getTransactionAmount(false, DoubloonToken, ETH);
+    expect(result).toBe(expectedResult);
+  });
 });
 
 //TODO: Missing null check on symbol
 describe("getReceivedAmount", () => {
-  test("IS buying -- returns exact received", () => {});
-  test("IS NOT buying -- returns exact paid", () => {});
+  test("IS buying -- returns exact received", () => {
+    const expectedResult = "Exact DBL Received";
+    const result = getReceivedAmount(true, DoubloonToken, ETH);
+    expect(result).toBe(expectedResult);
+  });
+  test("IS NOT buying -- returns exact paid", () => {
+    const expectedResult = "Exact ETH Paid";
+    const result = getReceivedAmount(false, DoubloonToken, ETH);
+    expect(result).toBe(expectedResult);
+  });
 });
 
 describe("getTradeInfoData0x", () => {
