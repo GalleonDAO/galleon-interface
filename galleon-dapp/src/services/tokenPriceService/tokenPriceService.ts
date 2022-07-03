084e27c8f6f4d395a2ea3073b230b21c7486c949
crown_dev <crown@galleon.community>
2022-07-03 14:41:01 +0100

    token price service

--- galleon-dapp/src/services/tokenPriceService/tokenPriceService.ts
+++ galleon-dapp/src/services/tokenPriceService/tokenPriceService.ts
@@ -1,5 +1,3 @@
-import { v4 as guid } from "uuid";
-
 import { Token } from "constants/tokens";
 import {
   coingeckoAPI,
@@ -7,42 +5,73 @@ import {
 } from "./sources/coingeckoAPI";
 import { tokensetsAPI } from "./sources/tokensetsAPI";
 import { loggingInstance } from "hooks/useLogging";
-const { captureDurationAsync, logTimer, logMessage, LOG_SEVERITY } =
-  loggingInstance();
+const { logMessage, LOG_SEVERITY } = loggingInstance();
 
 const DEFAULT_CURRENCY = "usd";
 
-export const getTokenMarketDataAsync = async (token: Token) => {
-  const correlationId = guid();
-  const timedResponse = await captureDurationAsync<HistoricalTokenMarketData>(
-    async (): Promise<HistoricalTokenMarketData> => {
-      coingeckoAPI
-        .getHistoricalTokenMarketDataAsync(
-          token.coingeckoId,
-          DEFAULT_CURRENCY,
-          correlationId
-        )
-        .then((result) => {
-          if (!result.marketcaps) {
-            tokensetsAPI
-              .getTokenPricesAsync(token.tokensetsId, correlationId)
-              .then((data) => {
-                result.marketcaps[0] = data.marketCap;
-              });
-          }
-          return result;
-        })
-        .catch((err) => {
-          logMessage(
-            LOG_SEVERITY.ERROR,
-            err.stack,
-            `Error Fetching Token Market Data, inner error: ${err.message}`,
-            correlationId,
-            getTokenMarketDataAsync.name
-          );
-        });
-      logTimer("GET_TOKEN_MARKET_DATA", timedResponse.duration);
-      return timedResponse.result;
-    }
-  );
+export const getTokenMarketDataAsync = async (
+  token: Token,
+  correlationId?: string
+): Promise<HistoricalTokenMarketData> => {
+  return coingeckoAPI
+    .getHistoricalTokenMarketDataAsync(
+      token.coingeckoId,
+      DEFAULT_CURRENCY,
+      correlationId
+    )
+    .catch<HistoricalTokenMarketData>((err) => {
+      logMessage(
+        LOG_SEVERITY.ERROR,
+        err.stack,
+        `Error Fetching Token Market Data, inner error: ${err.message}`,
+        correlationId,
+        getTokenMarketDataAsync.name
+      );
+      return {
+        hourlyPrices: [],
+        marketcaps: [],
+        volumes: [],
+      };
+    });
+};
+
+const getTokenMarketCapAsync = async (
+  token: Token,
+  correlationId?: string
+): Promise<number> => {
+  return tokensetsAPI
+    .getTokenPricesAsync(token.tokensetsId, correlationId)
+    .then((data) => {
+      return data.marketCap;
+    })
+    .catch<number>((err) => {
+      logMessage(
+        LOG_SEVERITY.ERROR,
+        err.stack,
+        `Error Fetching Token Market Cap, inner error: ${err.message}`,
+        correlationId,
+        getTokenMarketCapAsync.name
+      );
+      return 0;
+    });
+};
+
+export const getValidatedMarketCapAsync = async (
+  token: Token,
+  marketCaps: number[],
+  correlationId?: string
+): Promise<number[]> => {
+  if (marketCaps && marketCaps?.[marketCaps.length - 1] !== 0) {
+    return marketCaps;
+  }
+
+  const latestMC = await getTokenMarketCapAsync(token, correlationId);
+  if (!marketCaps) {
+    return [latestMC];
+  }
+
+  marketCaps?.[marketCaps.length - 1]
+    ? (marketCaps[marketCaps.length - 1] = latestMC)
+    : (marketCaps[0] = latestMC);
+  return marketCaps;
 };
